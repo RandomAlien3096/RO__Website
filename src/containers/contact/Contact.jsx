b86d601ff56797d8b66351178daa7a5ff9baeb75
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-01-10 23:45:09 -0600

    starting contact card info

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -3,7 +3,18 @@ import './contact.css';
 
 const Contact = () => {
   return (
-    <div></div>
+    <div className='RO__contact'>
+      <div className='RO__contact-content'>
+        <div className='RO__contact-content_shape'>
+          <div className='RO__contact-content_shape-title'></div>
+          <div className='RO__contact-content_shape-text'></div>
+          <div className='RO__contact-content_shape-button'></div>
+        </div>
+        <div className='RO__contact-content_text'>
+          <p>Make your project my next obsession.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
