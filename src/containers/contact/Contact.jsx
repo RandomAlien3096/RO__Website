df4fef904ed821e2d6508ad8360abc539fba50f1
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-01-11 17:40:06 -0600

    comiting contact part of the web page

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -5,14 +5,17 @@ const Contact = () => {
   return (
     <div className='RO__contact'>
       <div className='RO__contact-content'>
-        <div className='RO__contact-content_shape'>
-          <div className='RO__contact-content_shape-title'></div>
-          <div className='RO__contact-content_shape-text'></div>
-          <div className='RO__contact-content_shape-button'></div>
-        </div>
         <div className='RO__contact-content_text'>
           <p>Make your project my next obsession.</p>
         </div>
+        <div className='RO__contact-content_shape'>
+          <div className='RO__contact-content_shape-title'>
+            <p>Let's talk</p> 
+          </div>
+          <div className='RO__contact-content_shape-text'></div>
+          <div className='RO__contact-content_shape-button'></div>
+        </div>
+        
       </div>
     </div>
   )
--- src/containers/portfolio/Portfolio.jsx
+++ src/containers/portfolio/Portfolio.jsx
@@ -1,38 +1,40 @@
 import React from 'react';
 import './portfolio.css';
 
-const wrapper = document.getElementById("wrapper");
-
 const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-const uniqueRand = (min, max, prev) => {
-  let next = prev;
-  while(prev === next) next = rand(min, max);
-  
-  return next;
-}
+  const uniqueRand = (min, max, prev) => {
+    let next = prev;
+    while(prev === next) next = rand(min, max);
+    
+    return next;
+  }
+  const combinations = [
+    { configuration: 1, roundness: 1 },
+    { configuration: 1, roundness: 2 },
+    { configuration: 1, roundness: 3 },
+    { configuration: 2, roundness: 2 },
+    { configuration: 2, roundness: 3 }
+  ];
 
-const combinations = [
-  { configuration: 1, roundness: 1 },
-  { configuration: 1, roundness: 2 },
-  { configuration: 1, roundness: 3 },
-  { configuration: 2, roundness: 2 },
-  { configuration: 2, roundness: 3 }
-];
+  let prev = 0;
 
-let prev = 0;
-
-setInterval(() => {
-  const index = uniqueRand(0, combinations.length - 1, prev),
-        combination = combinations[index];
   
-  wrapper.dataset.configuration = combination.configuration;
-  wrapper.dataset.roundness = combination.roundness;
-  
-  prev = index;
-}, 3000);
 
 const Portfolio = () => {
+
+  const wrapper = document.getElementById("wrapper");
+
+  setInterval(() => {
+    const index = uniqueRand(0, combinations.length - 1, prev),
+          combination = combinations[index];
+    
+    wrapper.dataset.configuration = combination.configuration;
+    wrapper.dataset.roundness = combination.roundness;
+    
+    prev = index;
+  }, 3000);
+  
   return (
     <div className='RO__portfolio'>
       <div className='RO__portfolio-content' data-roundness="1" data-configuration ="1" id='wrapper'>
@@ -46,6 +48,7 @@ const Portfolio = () => {
       </div>
     </div>
   )
+
 }
 
 export default Portfolio
