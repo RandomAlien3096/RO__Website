03ebc5513d5f762f5c32ec05348b047e792f3de2
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-01-10 21:04:24 -0600

    changes to portfolio gadget

--- src/containers/portfolio/Portfolio.jsx
+++ src/containers/portfolio/Portfolio.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import './portfolio.css';
 
+const wrapper = document.getElementById("wrapper");
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const uniqueRand = (min, max, prev) => {
+  let next = prev;
+  
+  while(prev === next) next = rand(min, max);
+  
+  return next;
+}
+
+const combinations = [
+  { configuration: 1, roundness: 1 },
+  { configuration: 1, roundness: 2 },
+  { configuration: 1, roundness: 3 },
+  { configuration: 2, roundness: 2 },
+  { configuration: 2, roundness: 3 }
+];
+
+let prev = 0;
+
+setInterval(() => {
+  const index = uniqueRand(0, combinations.length - 1, prev),
+        combination = combinations[index];
+  
+  wrapper.dataset.configuration = combination.configuration;
+  wrapper.dataset.roundness = combination.roundness;
+  
+  prev = index;
+}, 3000);
+
 const Portfolio = () => {
   return (
     <div className='RO__portfolio'>
-      <div className='RO__portfolio-content'>
+      <div className='RO__portfolio-content' data-roundness="1" data-configuration ="1" id='wrapper'>
         <div className='RO__portfolio-content_shape'></div>
         <div className='RO__portfolio-content_shape'></div>
         <div className='RO__portfolio-content_shape'></div>
