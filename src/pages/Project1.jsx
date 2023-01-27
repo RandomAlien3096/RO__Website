3ef5dff9f0cd47282b01c119ad12d214a51dca7f
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-01-27 17:10:59 -0600

    trying to implement jupyter

--- src/pages/Project1.jsx
+++ src/pages/Project1.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { IpynbRenderer } from 'react-ipynb-renderer';
+import "react-ipynb-renderer/dist/styles/monokai.css"
+
 import './project1.css';
+import ipynb from './TelstraNotebookCatboost.ipynb';
+
 
 const Project1 = () => {
   return (
-    <div>project1</div>
-  )
+    <div>
+      <IpynbRenderer ipynb={ipynb} syntaxTheme={"okaidia"} />
+      Project 1: Telstra Notebook with Catboost Model
+    </div>
+  );
 }
 
 export default Project1
