63ffbd2bab44060c88c0ddcf0fbc0df0bde7892e
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-01-27 20:27:15 -0600

    Coming soon page

--- src/pages/Project1.jsx
+++ src/pages/Project1.jsx
@@ -8,9 +8,30 @@ import ipynb from './TelstraNotebookCatboost.ipynb';
 
 const Project1 = () => {
   return (
-    <div>
-      <IpynbRenderer ipynb={ipynb} syntaxTheme={"okaidia"} />
-      Project 1: Telstra Notebook with Catboost Model
+    <div className='RO__ComingSoon'>
+      <div className='RO__ComingSoon-content'>
+        <p>Page is under construction</p>
+        <h1>Launching Soon this Project</h1>
+      </div>
+      <div className='RO__ComingSoon-timer'>
+        <div className='RO__ComingSoon-timer_Days'>
+          <p>00</p>
+          <span>Days</span>
+        </div>
+        <div className='RO__ComingSoon-timer_Hours'>
+          <p>00</p>
+          <span>Hours</span>
+        </div>
+        <div className='RO__ComingSoon-timer_Minutes'>
+          <p>00</p>
+          <span>Minutes</span>
+        </div>
+        <div className='RO__ComingSoon-timer_Seconds'>
+          <p>00</p>
+          <span>Seconds</span>
+        </div>
+      </div>
+      
     </div>
   );
 }
