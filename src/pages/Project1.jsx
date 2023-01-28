98bb09e636f32f8fe107697bc84643382e8522d3
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-01-27 21:23:23 -0600

    Count down timer

--- src/pages/Project1.jsx
+++ src/pages/Project1.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import { IpynbRenderer } from 'react-ipynb-renderer';
 import "react-ipynb-renderer/dist/styles/monokai.css"
+import { useEffect, useState } from 'react';
 
 import './project1.css';
 import ipynb from './TelstraNotebookCatboost.ipynb';
 
+
+const useCountdown = (targetDate) => {
+  const countDownDate = new Date(targetDate).getTime();
+
+  const [countDown, setCountDown] = useState(
+    countDownDate - new Date().getTime()
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountDown(countDownDate - new Date().getTime());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [countDownDate]);
+
+  return getReturnValues(countDown)
+};
+
+const getReturnValues = (countDown) => {
+  // calculate time left
+  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+
+  return [days, hours, minutes, seconds];
+};
+
+export { useCountdown };
 
 const Project1 = () => {
   return (
@@ -31,7 +64,9 @@ const Project1 = () => {
           <span>Seconds</span>
         </div>
       </div>
-      
+      <div className='RO__ComingSoon-button'>
+          <button type='button'>Go back to Landingpage</button>
+      </div>
     </div>
   );
 }
