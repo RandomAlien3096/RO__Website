de07a6a937671ba9c955ae3f8df14eb4c70b8a4f
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-02-03 21:20:57 -0600

    Contact poge

--- src/pages/contactform/ContactForm.jsx
+++ src/pages/contactform/ContactForm.jsx
@@ -28,44 +28,56 @@ const ContactForm = () => {
             <h3>Contact</h3>
             <h1>I'm here to help you level up</h1>
         </div>
-        <div className='RO__ContactForm-description'>
-            <p>I'm just on click away to help you take your company 
-                to the next level. Fill in the form to share more 
-                details about the project or your favorite movie. 
-                Either way, I'd love to talk.</p>
-        </div>
-        <form
-            className='RO__ContactForm-form'
-            action = {FORM_ENDPOINT}
-            onSubmit = {handleSubmit}
-            method = 'POST'
-            target='_blank'
-            >
-            <div className='RO__ContactForm-form_name'>
-                <input 
-                    type= 'text'
-                    placeholder='Your name' 
-                    name='name' required 
-                />
+        <div className='RO__ContactForm-content'>
+            <div className='RO__ContactForm-content_description'>
+                <p>I'm just on click away to help you take your company 
+                    to the next level. Fill in the form to share more 
+                    details about the project or your favorite movie. 
+                    Either way, I'd love to talk.</p>
             </div>
-            <div className='RO__ContactForm-form_email'>
-                <input
-                    type='email'
-                    placeholder='Your email'
-                    name='email' required
-                />
-            </div>
-            <div className='RO__ContactForm-form_info'>
-                <textarea
-                    placeholder='What can I do for you?'
-                    name='message' required 
-                />
-            </div>
-            <div className='RO__ContactForm-form_button'>
-                <button type='button'>Send</button>
-            </div>
+            <form
+                className='RO__ContactForm-content_form'
+                action = {FORM_ENDPOINT}
+                onSubmit = {handleSubmit}
+                method = 'POST'
+                target='_blank'
+                >
+                <div className='RO__ContactForm-content_form_name'>
+                    <div className='RO__ContactForm-content_form_nameTitle'>
+                        <h5>What's your name?</h5>
+                    </div>
+                    <input 
+                        className='RO_ContactForm-content_form_nameInput'
+                        type= 'text'
+                        name='name' required 
+                    />
+                </div>
+                <div className='RO__ContactForm-content_form_email'>
+                    <div className='RO__ContactForm-content_form_emailTitle'>
+                        <h5>Your email</h5>
+                    </div>
+                    <input
+                        className='RO__ContactForm-content_form_emailInput'
+                        type='email'
+                        name='email' required
+                    />
+                </div>
+                <div className='RO__ContactForm-content_form_info'>
+                    <div className='RO__ContactForm-content_form_infoTitle'>
+                        <h5>What can I help you with?</h5>
+                    </div>
+                    <textarea
+                        className='RO__ContactForm-content_form_infoContent'
+                        name='message' required 
+                    />
+                </div>
+                <div className='RO__ContactForm-content_form_button'>
+                    <button type='button'>Send</button>
+                </div>
 
-        </form>
+            </form>
+        </div>
+        
     </div>
   )
 }
